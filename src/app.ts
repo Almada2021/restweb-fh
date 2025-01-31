bf46d5b45e858737a24d73032f9039d6d08e55b3
Tobias Almada <talmada908@gmail.com>
2025-01-31 14:21:27 -0300

    ready

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,11 @@
-import fs from "fs";
-import http2 from "http2";
+import { Server } from "./presentation/server";
 
-const server = http2.createSecureServer(
-  {
-    key: fs.readFileSync("./keys/server.key"),
-    cert: fs.readFileSync("./keys/server.crt"),
-  },
-  (req, res) => {
-    console.log(req.url);
-    // res.writeHead(200, {
-    // "Content-Type": "text/html",
-    // });
+(() => {
+  main();
+})();
 
-    // SSR
-    // res.write(`<h1 style='background-color:#f00'>${req.url}</h1>`);
-    const exp = /\.(css|js)/;
-    if (req.url == "/") {
-      const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
-      res.writeHead(200, {
-        "Content-Type": "text/html",
-      });
-      res.end(htmlFile);
-      return;
-    } else if (exp.test(req.url!)) {
-      const file = fs.readFileSync(`./public${req.url}`, "utf-8");
-
-      res.end(file);
-    } else {
-      res.writeHead(404, {
-        "Content-Type": "text/html",
-      });
-      res.end();
-    }
-
-    // const data = { name: "Jonh Doe", age: 30, city: "New York" };
-    // res.writeHead(200, {
-    //   "Content-Type": "application/json",
-    // });
-    // res.write(JSON.stringify(data));
-    // res.end();
-  }
-);
-server.listen(3000);
+function main() {
+  const server = new Server();
+  server.start();
+  console.log("here");
+}
