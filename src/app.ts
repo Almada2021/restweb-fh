dc770e4038a5ba94b12e1dddbdc8c523656dfe99
Tobias Almada <talmada908@gmail.com>
2025-01-31 14:02:53 -0300

    Working

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,44 @@
 import fs from "fs";
-import http from "http";
+import http2 from "http2";
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  // res.writeHead(200, {
-  // "Content-Type": "text/html",
-  // });
+const server = http2.createSecureServer(
+  {
+    key: fs.readFileSync("./keys/server.key"),
+    cert: fs.readFileSync("./keys/server.crt"),
+  },
+  (req, res) => {
+    console.log(req.url);
+    // res.writeHead(200, {
+    // "Content-Type": "text/html",
+    // });
 
-  // SSR
-  // res.write(`<h1 style='background-color:#f00'>${req.url}</h1>`);
-  const exp = /\.(css|js)/;
-  if (req.url == "/") {
-    const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    res.end(htmlFile);
-    return;
-  } else if (exp.test(req.url!)) {
-    const file = fs.readFileSync(`./public${req.url}`, "utf-8");
-    res.end(file);
-  } else {
-    res.writeHead(404, {
-      "Content-Type": "text/html",
-    });
-    res.end("Not Found");
+    // SSR
+    // res.write(`<h1 style='background-color:#f00'>${req.url}</h1>`);
+    const exp = /\.(css|js)/;
+    if (req.url == "/") {
+      const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.end(htmlFile);
+      return;
+    } else if (exp.test(req.url!)) {
+      const file = fs.readFileSync(`./public${req.url}`, "utf-8");
+
+      res.end(file);
+    } else {
+      res.writeHead(404, {
+        "Content-Type": "text/html",
+      });
+      res.end();
+    }
+
+    // const data = { name: "Jonh Doe", age: 30, city: "New York" };
+    // res.writeHead(200, {
+    //   "Content-Type": "application/json",
+    // });
+    // res.write(JSON.stringify(data));
+    // res.end();
   }
-
-  // const data = { name: "Jonh Doe", age: 30, city: "New York" };
-  // res.writeHead(200, {
-  //   "Content-Type": "application/json",
-  // });
-  // res.write(JSON.stringify(data));
-  // res.end();
-});
+);
 server.listen(3000);
